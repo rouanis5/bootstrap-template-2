2840abe7edb5ea990bb48989ad7047e370a2ee5a
rouanis5 <ouanis42@gmail.com>
2022-03-10 18:56:56 +0100

    + sass

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
 import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../node_modules/bootstrap/dist/js/bootstrap";
+import "./scss/style.scss";
